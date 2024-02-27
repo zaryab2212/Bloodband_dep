a8a0b083dccbc27ce9e2a39c21d57691dd86ad00
zaryab ikram <zaryabsheikh0@gmail.com>
2024-02-27 10:47:59 +0500

    deply

--- Config/connection.js
+++ Config/connection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const database = async function main() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://jessiep1232:hrjcWb4bAyCpUFYJ@blood-bank.2v5b4np.mongodb.net/bank?retryWrites=true&w=majority"
+    );
     console.log("data base connted");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   } catch (error) {
